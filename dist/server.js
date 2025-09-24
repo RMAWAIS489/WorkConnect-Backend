98d05fd20390f8faf4018e3feb1774a02414e9c7
RMAWAIS489 <rmawais489@gmail.com>
2025-09-24 16:54:47 +0500

    some changes for vercel

--- dist/server.js
+++ dist/server.js
@@ -3,14 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 import { AppDataSource } from "./database/data-source.js";
 import app from "./app.js";
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 AppDataSource.initialize()
     .then(() => {
     console.log("Database connected successfully");
-    app.listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
-    });
+    // app.listen(PORT, () => {
+    //   console.log(`Server running on http://localhost:${PORT}`);
+    // });
 })
     .catch((error) => {
     console.log("Database connection error:", error);
 });
+export default app;
