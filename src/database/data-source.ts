d7dbe6b73c119927d61536c8639e7fbdcce64d9a
RMAWAIS489 <rmawais489@gmail.com>
2025-09-25 14:43:40 +0500

    some major changes

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,11 +2,24 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  url: process.env.DATABASE_URL,  // use env var
+  url: process.env.DATABASE_URL, // Neon connection string
   synchronize: false,
   logging: false,
   ssl: { rejectUnauthorized: false }, // needed for Neon
-  entities: ["dist/entity/**/*{.ts,.js}"],
-  migrations: ["dist/migration/**/*{.ts,.js}"],
-  subscribers: ["dist/subscriber/**/*{.ts,.js}"],
+
+  entities: [
+    process.env.NODE_ENV === "production"
+      ? "dist/entity/**/*{.js}" // only JS in production
+      : "src/entity/**/*{.ts}", // TS in dev
+  ],
+  migrations: [
+    process.env.NODE_ENV === "production"
+      ? "dist/migration/**/*{.js}"
+      : "src/migration/**/*{.ts}",
+  ],
+  subscribers: [
+    process.env.NODE_ENV === "production"
+      ? "dist/subscriber/**/*{.js}"
+      : "src/subscriber/**/*{.ts}",
+  ],
 });
